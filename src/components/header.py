b4c70b949932e616ad40d2c8843f044fc4c87632
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-10-02 23:01:10 -0300

    feat: usando CTkFont para colocar a font no header

--- src/components/header.py
+++ src/components/header.py
@@ -3,7 +3,7 @@ import customtkinter as ctk
 
 class Header:
     def __init__(self, window):
-        self.fonte_cabecalho = ("Garamond", 24, "bold")
+        self.fonte_cabecalho = ctk.CTkFont("Garamond", 24, "bold")
         self.cabecalho = ctk.CTkLabel(
             window,
             font=self.fonte_cabecalho,
